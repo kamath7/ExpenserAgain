068c9d6b983ecbc540946edb7dd1c45f96f14c90
Adithya <adithyakamath96@gmail.com>
2024-11-08 03:02:36 +0530

    props

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SimpleForm from './SimpleForm';
+import ExpenseList from './ExpenseList';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [expenses, setExpenses] = useState([]);
+
+  const addExpense = (name, amount, date) => {
+    setExpenses(prevExpenses => [
+      ...prevExpenses,
+      { id: Date.now().toString(), name, amount, date: date.toLocaleDateString() },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
    
-        <SimpleForm />
+        <SimpleForm addExpense={addExpense}/>
+        <ExpenseList expenses={expenses}/>
     </View>
   );
 }
